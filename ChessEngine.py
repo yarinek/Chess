b7da67521394c74aa4ab9c2b26f8d7db8bc3b2d8
yarinek <mareks726@gmail.com>
2021-02-24 11:36:07 +0100

    +init board

---
+++ ChessEngine.py
@@ -0,0 +1,17 @@
+# Storing all information about state of game
+
+class GameState():
+    def __init__(self):
+        # Board
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
---
+++ ChessMain.py
@@ -0,0 +1,69 @@
+import pygame as p
+import ChessEngine
+
+
+# Global variables
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT//DIMENSION
+IMAGES = {}
+
+# Init images
+def load_images():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wQ',
+              'wK', 'bp', 'bR', 'bN', 'bB', 'bQ', 'bK']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load(
+            "images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+# MAIN
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    load_images()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(15)
+        p.display.flip()
+
+
+# Graphic
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    # later
+    drawPieces(screen, gs.board)
+
+# Draw the squares on the board
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(
+                c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+# Draw the pieces on the board
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != '--':  # not empty square
+                screen.blit(IMAGES[piece], p.Rect(
+                    c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == "__main__":
+    main()
