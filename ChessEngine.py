5d440b56c70ab900266e8660737f1a1e89d015ae
yarinek <mareks726@gmail.com>
2021-02-24 17:27:59 +0100

    +Moves

--- ChessEngine.py
+++ ChessEngine.py
@@ -15,3 +15,35 @@ class GameState():
         ]
         self.whiteToMove = True
         self.moveLog = []
+
+    def make_move(self, move):
+        self.board[move.startRow][move.startCol] = '--'
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)  # log the move
+        self.whiteToMove = not self.whiteToMove  # swap players
+
+
+class Move():
+    # map keys to values
+    # key:value
+    ranks_to_rows = {"1": 7, "2": 6, "3": 5,
+                     "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
+
+    files_to_cols = {"h": 7, "g": 6, "f": 5,
+                     "e": 4, "d": 3, "c": 2, "b": 1, "a": 0}
+    cols_to_files = {v: k for k, v in files_to_cols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def get_chess_notation(self):
+        return self.get_rank_file(self.startRow, self.startCol) + self.get_rank_file(self.endRow, self.endCol)
+
+    def get_rank_file(self, r, c):
+        return self.cols_to_files[c]+self.rows_to_ranks[r]
--- ChessMain.py
+++ ChessMain.py
@@ -1,4 +1,4 @@
-import pygame as p
+import pygame
 import ChessEngine
 
 
@@ -9,31 +9,53 @@ SQ_SIZE = HEIGHT//DIMENSION
 IMAGES = {}
 
 # Init images
+
+
 def load_images():
     pieces = ['wp', 'wR', 'wN', 'wB', 'wQ',
               'wK', 'bp', 'bR', 'bN', 'bB', 'bQ', 'bK']
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load(
+        IMAGES[piece] = pygame.transform.scale(pygame.image.load(
             "images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
 
 
 # MAIN
 
 def main():
-    p.init()
-    screen = p.display.set_mode((WIDTH, HEIGHT))
-    clock = p.time.Clock()
-    screen.fill(p.Color("white"))
-    gs = ChessEngine.GameState()
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    screen.fill(pygame.Color("white"))
+    gs = ChessEngine.GameState()  # GAME_STATE
     load_images()
     running = True
+    sqSelected = ()  # Square clicked by user tuple(row, col)
+    playerClicks = []  # Where user clicked after list[(row, col), (row2,col2)]
+
     while running:
-        for e in p.event.get():
-            if e.type == p.QUIT:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
                 running = False
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                location = pygame.mouse.get_pos()  # (x,y) location of mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col):  # The user clicked the same square twice
+                    sqSelected = ()  # unselect
+                    playerClicks = []  # clear clicks
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:  # after 2nd click
+                    move = ChessEngine.Move(
+                        playerClicks[0], playerClicks[1], gs.board)
+                    print(move.get_chess_notation())
+                    gs.make_move(move)
+                    sqSelected = ()  # Reset clicks
+                    playerClicks = []  # Reset clicks
         drawGameState(screen, gs)
         clock.tick(15)
-        p.display.flip()
+        pygame.display.flip()
 
 
 # Graphic
@@ -46,11 +68,11 @@ def drawGameState(screen, gs):
 
 
 def drawBoard(screen):
-    colors = [p.Color("white"), p.Color("gray")]
+    colors = [pygame.Color("white"), pygame.Color("gray")]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = colors[((r+c) % 2)]
-            p.draw.rect(screen, color, p.Rect(
+            pygame.draw.rect(screen, color, pygame.Rect(
                 c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 # Draw the pieces on the board
@@ -61,7 +83,7 @@ def drawPieces(screen, board):
         for c in range(DIMENSION):
             piece = board[r][c]
             if piece != '--':  # not empty square
-                screen.blit(IMAGES[piece], p.Rect(
+                screen.blit(IMAGES[piece], pygame.Rect(
                     c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
